fd7d0b39680dfc140d420dfb09e3941b796d04b7
himihiba <hiba.himi@ensttic.dz>
2024-12-17 21:23:33 +0100

    use product.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,35 +1,5 @@
 // Save the data
-const products = [{
-	image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-	name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-	rating: {
-		stars: 4.5,
-		count: 87
-	},
-	// on sentce
-	price: 1090 
-},
-{
-	image: 'images/products/intermediate-composite-basketball.jpg',
-	name: 'Intermediate Size Basketball',
-	rating: {
-		stars: 4,
-		count: 127
-	},
-	// on sentce
-	price: 2095 
-},
-{
-	image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-	name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-	rating: {
-		stars: 4.5,
-		count: 56
-	},
-	// on sentce
-	price: 799 
-}
-];
+
 
 // generate the html
 let productsHTML = '';
